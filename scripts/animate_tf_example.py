44923dfd8f42824032388f27b52d27b6ff8e2903
karthik <karthik4294@gmail.com>
2025-03-09 22:29:35 -0700

    separate out features desc

--- scripts/animate_tf_example.py
+++ scripts/animate_tf_example.py
@@ -4,148 +4,16 @@ sys.path.append('../')
 from kv_transformer.utils.viz import animate_scenario
 import tensorflow as tf
 from IPython.core.display import display, HTML
-
-# If you use a custom conversion from Scenario to tf.Example, set the correct
-# number of map samples here.
-num_map_samples = 30000
-
-# Example field definition
-roadgraph_features = {
-    'roadgraph_samples/dir': tf.io.FixedLenFeature(
-        [num_map_samples, 3], tf.float32, default_value=None
-    ),
-    'roadgraph_samples/id': tf.io.FixedLenFeature(
-        [num_map_samples, 1], tf.int64, default_value=None
-    ),
-    'roadgraph_samples/type': tf.io.FixedLenFeature(
-        [num_map_samples, 1], tf.int64, default_value=None
-    ),
-    'roadgraph_samples/valid': tf.io.FixedLenFeature(
-        [num_map_samples, 1], tf.int64, default_value=None
-    ),
-    'roadgraph_samples/xyz': tf.io.FixedLenFeature(
-        [num_map_samples, 3], tf.float32, default_value=None
-    ),
-}
-# Features of other agents.
-state_features = {
-    'state/id':
-        tf.io.FixedLenFeature([128], tf.float32, default_value=None),
-    'state/type':
-        tf.io.FixedLenFeature([128], tf.float32, default_value=None),
-    'state/is_sdc':
-        tf.io.FixedLenFeature([128], tf.int64, default_value=None),
-    'state/tracks_to_predict':
-        tf.io.FixedLenFeature([128], tf.int64, default_value=None),
-    'state/current/bbox_yaw':
-        tf.io.FixedLenFeature([128, 1], tf.float32, default_value=None),
-    'state/current/height':
-        tf.io.FixedLenFeature([128, 1], tf.float32, default_value=None),
-    'state/current/length':
-        tf.io.FixedLenFeature([128, 1], tf.float32, default_value=None),
-    'state/current/timestamp_micros':
-        tf.io.FixedLenFeature([128, 1], tf.int64, default_value=None),
-    'state/current/valid':
-        tf.io.FixedLenFeature([128, 1], tf.int64, default_value=None),
-    'state/current/vel_yaw':
-        tf.io.FixedLenFeature([128, 1], tf.float32, default_value=None),
-    'state/current/velocity_x':
-        tf.io.FixedLenFeature([128, 1], tf.float32, default_value=None),
-    'state/current/velocity_y':
-        tf.io.FixedLenFeature([128, 1], tf.float32, default_value=None),
-    'state/current/width':
-        tf.io.FixedLenFeature([128, 1], tf.float32, default_value=None),
-    'state/current/x':
-        tf.io.FixedLenFeature([128, 1], tf.float32, default_value=None),
-    'state/current/y':
-        tf.io.FixedLenFeature([128, 1], tf.float32, default_value=None),
-    'state/current/z':
-        tf.io.FixedLenFeature([128, 1], tf.float32, default_value=None),
-    'state/future/bbox_yaw':
-        tf.io.FixedLenFeature([128, 80], tf.float32, default_value=None),
-    'state/future/height':
-        tf.io.FixedLenFeature([128, 80], tf.float32, default_value=None),
-    'state/future/length':
-        tf.io.FixedLenFeature([128, 80], tf.float32, default_value=None),
-    'state/future/timestamp_micros':
-        tf.io.FixedLenFeature([128, 80], tf.int64, default_value=None),
-    'state/future/valid':
-        tf.io.FixedLenFeature([128, 80], tf.int64, default_value=None),
-    'state/future/vel_yaw':
-        tf.io.FixedLenFeature([128, 80], tf.float32, default_value=None),
-    'state/future/velocity_x':
-        tf.io.FixedLenFeature([128, 80], tf.float32, default_value=None),
-    'state/future/velocity_y':
-        tf.io.FixedLenFeature([128, 80], tf.float32, default_value=None),
-    'state/future/width':
-        tf.io.FixedLenFeature([128, 80], tf.float32, default_value=None),
-    'state/future/x':
-        tf.io.FixedLenFeature([128, 80], tf.float32, default_value=None),
-    'state/future/y':
-        tf.io.FixedLenFeature([128, 80], tf.float32, default_value=None),
-    'state/future/z':
-        tf.io.FixedLenFeature([128, 80], tf.float32, default_value=None),
-    'state/past/bbox_yaw':
-        tf.io.FixedLenFeature([128, 10], tf.float32, default_value=None),
-    'state/past/height':
-        tf.io.FixedLenFeature([128, 10], tf.float32, default_value=None),
-    'state/past/length':
-        tf.io.FixedLenFeature([128, 10], tf.float32, default_value=None),
-    'state/past/timestamp_micros':
-        tf.io.FixedLenFeature([128, 10], tf.int64, default_value=None),
-    'state/past/valid':
-        tf.io.FixedLenFeature([128, 10], tf.int64, default_value=None),
-    'state/past/vel_yaw':
-        tf.io.FixedLenFeature([128, 10], tf.float32, default_value=None),
-    'state/past/velocity_x':
-        tf.io.FixedLenFeature([128, 10], tf.float32, default_value=None),
-    'state/past/velocity_y':
-        tf.io.FixedLenFeature([128, 10], tf.float32, default_value=None),
-    'state/past/width':
-        tf.io.FixedLenFeature([128, 10], tf.float32, default_value=None),
-    'state/past/x':
-        tf.io.FixedLenFeature([128, 10], tf.float32, default_value=None),
-    'state/past/y':
-        tf.io.FixedLenFeature([128, 10], tf.float32, default_value=None),
-    'state/past/z':
-        tf.io.FixedLenFeature([128, 10], tf.float32, default_value=None),
-}
-
-traffic_light_features = {
-    'traffic_light_state/current/state':
-        tf.io.FixedLenFeature([1, 16], tf.int64, default_value=None),
-    'traffic_light_state/current/valid':
-        tf.io.FixedLenFeature([1, 16], tf.int64, default_value=None),
-    'traffic_light_state/current/x':
-        tf.io.FixedLenFeature([1, 16], tf.float32, default_value=None),
-    'traffic_light_state/current/y':
-        tf.io.FixedLenFeature([1, 16], tf.float32, default_value=None),
-    'traffic_light_state/current/z':
-        tf.io.FixedLenFeature([1, 16], tf.float32, default_value=None),
-    'traffic_light_state/past/state':
-        tf.io.FixedLenFeature([10, 16], tf.int64, default_value=None),
-    'traffic_light_state/past/valid':
-        tf.io.FixedLenFeature([10, 16], tf.int64, default_value=None),
-    'traffic_light_state/past/x':
-        tf.io.FixedLenFeature([10, 16], tf.float32, default_value=None),
-    'traffic_light_state/past/y':
-        tf.io.FixedLenFeature([10, 16], tf.float32, default_value=None),
-    'traffic_light_state/past/z':
-        tf.io.FixedLenFeature([10, 16], tf.float32, default_value=None),
-}
+from kv_transformer.utils.data import features_description
 
 def display_tf_example(file_path):
-    features_description = {}
-    features_description.update(roadgraph_features)
-    features_description.update(state_features)
-    features_description.update(traffic_light_features)
-
+    fd = features_description.get_features_description()
     if file_path == '':
         file_path = './data/uncompressed/tf_example/training/training_tfexample.tfrecord-00906-of-01000'
 
     dataset = tf.data.TFRecordDataset(file_path, compression_type='')
     data = next(dataset.as_numpy_iterator())
-    parsed = tf.io.parse_single_example(data, features_description)
+    parsed = tf.io.parse_single_example(data, fd)
 
     images = animate_scenario.visualize_all_agents_smooth(parsed)
     anim = animate_scenario.create_animation(images)
